ec1ce6b97a9e9a950ef42ff8cdbfa52a09b3eb63
Jennymurakami <jinmin1000903@gmail.com>
2022-07-16 17:23:00 +0900

    homework

--- all.js
+++ all.js
@@ -1,1 +1,26 @@
-let 
+$('.mb_list').slideUp(0);
+$('.mb_menu').click(function () {
+    $('.mb_list').slideDown(500);
+})
+
+
+$('*').each(function () {
+    $(this).click(function () {
+
+        let target = $(this).attr("data-gt-target");
+
+        if (target) {
+            let top = $(target).offset().top;
+            $('html').animate({
+                scrollTop: top
+            }), 1000
+        }
+    })
+
+
+})
+
+$('.mb_list').click(function () {
+    $('.mb_list').slideUp(500);
+})
+
